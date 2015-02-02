b0d5d633a97826e860d57634bda0a8d2db93a44b
ramesh7128 <ramesh7128@gmail.com>
2015-02-02 20:21:16 +0530

    templates and static folder

--- stackdjango/stackdjango/settings.py
+++ stackdjango/stackdjango/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'stackapp'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -81,3 +82,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
